eb49206e56f612a07434e77d46dcafbcf7ea1ad6
WIllframe <mozhevm@gmail.com>
2025-10-05 15:12:50 +0300

    Initial Commit

---
+++ Translate/TranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateApp.swift
+//  Translate
+//
+//  Created by Matvey Mozhevikin on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
